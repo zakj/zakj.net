7f32fe442e0e326ab3001dfdfd15ef463e00002d
Zak Johnson <me@zakj.net>
2016-09-25 18:14:06 -0700

    Framework for notes pages

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,25 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 var webpack = require('webpack');
-
 
 module.exports = {
   entry: {
     'styles': './sass/main.sass',
   },
+
+  output: {
+    path: path.join(path.dirname(__dirname), '/assets/gen'),
+    filename: '[name].js',
+  },
+
   module: {
     loaders: [
+      {
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel',
+      },
       {
         test: /\.sass$/,
         exclude: path.resolve(__dirname, 'node_modules'),
@@ -16,14 +27,22 @@ module.exports = {
       },
     ],
   },
-  output: {
-    path: path.join(path.dirname(__dirname), '/assets/gen'),
-    filename: '[name].js',
-  },
+
+  preLoaders: [
+    {
+      test: /\.js$/,
+      loader: 'source-map-loader',
+      exclude: path.resolve(__dirname, 'node_modules'),
+    },
+  ],
+
   plugins: [
+    new HtmlWebpackPlugin(),
     new ExtractTextPlugin('[name].css', {
       allChunks: true
     }),
     new webpack.optimize.UglifyJsPlugin(),
   ],
+
+  devtool: 'source-map',
 };
