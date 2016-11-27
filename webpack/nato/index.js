ab4001721a10fa74ba0dc6c03af091cf794ce030
Zak Johnson <me@zakj.net>
2016-11-26 21:29:26 -0800

    NATO maquette -> vue

--- webpack/nato/index.js
+++ webpack/nato/index.js
@@ -1,28 +1,5 @@
-import { createProjector, h } from 'maquette';
+import Vue from 'vue';
+import nato from './nato.vue';
+import './style.sass';
 
-import alphabet from './alphabet';
-import style from './style.sass';
-
-
-let input = '';
-
-function handleInput(e) {
-  input = e.target.value;
-}
-
-function mapInput(c) {
-  c = c.toLowerCase();
-  if (c in alphabet) return alphabet[c];
-  if (c.match(/\s/)) return ' ';
-  return c;
-}
-
-function render() {
-  return h('div.nato', [
-    h('input', {oninput: handleInput}),
-    h('div.result', input.split('').map((c, i) => h('div', {key: i}, [mapInput(c)]))),
-    h('ul.alphabet', Object.keys(alphabet).map(k => h('li', [alphabet[k]]))),
-  ]);
-}
-
-createProjector({}).append(document.body, render);
+new Vue(Object.assign({}, nato, {el: document.getElementById('root')}));
---
+++ webpack/nato/nato.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="nato">
+    <input v-model="text">
+    <div class="result">
+      <div v-for="code in codedText">{{ code }}</div>
+    </div>
+    <ul class="alphabet">
+      <li v-for="code in alphabet">{{ code }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import alphabet from './alphabet';
+
+  function mapInput(c) {
+    c = c.toLowerCase();
+    if (c in alphabet) return alphabet[c];
+    if (c.match(/\s/)) return ' ';
+    return c;
+  }
+
+  module.exports = {
+    data: function() {
+      return {
+        alphabet,
+        text: '',
+      };
+    },
+
+    computed: {
+      codedText: function () {
+        return this.text.split('').map(mapInput);
+      },
+    },
+  };
+</script>
