d212be174e3590a5e9397d8ce2022d64524d4dd2
Zak Johnson <me@zakj.net>
2020-07-10 09:13:12 -0700

    Set up next-images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  webpack: config => {
+const withImages = require('next-images');
+
+module.exports = withImages({
+  inlineImageLimit: 10000,
+  webpack: (config) => {
     config.devServer = config.devServer || {};
     config.devServer.host = '0.0.0.0';
     return config;
   },
-};
+});
