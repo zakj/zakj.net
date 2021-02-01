a62c86be9a323c56f3b896a33d96aa26a2c0fe0e
Zak Johnson <me@zakj.net>
2021-01-31 23:58:29 -0800

    Update nav implementation

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,24 +1,18 @@
 import { motion } from 'framer-motion';
-import { useEffect, useRef, useState, RefObject } from 'react';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useWindowSize } from 'react-use';
-import PropTypes from 'prop-types';
+import { SectionName, SectionRefs } from 'src/sections';
+import media from 'style/media';
 import styled from 'styled-components';
 
-import media from 'style/media';
-import { SectionName, SectionRefs } from 'src/sections';
-
-const MARKER_OFFSET = 1;
-const MARKER_LEFT = 4;
-
 interface Position {
-  left: number,
-  y: number,
-  scaleY: number,
+  top: number;
+  x: number;
+  scaleX: number;
 }
 
 interface Props {
-  sectionName: SectionName,
+  sectionName: SectionName;
   onClick: (sectionName: SectionName) => void;
 }
 
@@ -33,27 +27,28 @@ function Wrapper({ sectionName, onClick }: Props) {
   };
 
   useEffect(() => {
-    const topSection = sections[SectionName.Bio].current?.getBoundingClientRect();
+    const topSection = sections[
+      SectionName.Bio
+    ].current?.getBoundingClientRect();
     if (!topSection) return;
     const position = {
-      y: topSection.top,
-      scaleY: 0,
-      left: topSection.left - MARKER_LEFT,
+      x: topSection.top,
+      scaleX: 0,
+      top: topSection.width + itemPadding,
     };
-    const rect = sections[sectionName as keyof NavSectionRefs]?.current?.getBoundingClientRect();
+    const rect = sections[
+      sectionName as keyof NavSectionRefs
+    ]?.current?.getBoundingClientRect();
     if (rect) {
-      position.y = rect.top + MARKER_OFFSET;
-      position.scaleY = rect.height - MARKER_OFFSET;
+      position.x = rect.top;
+      position.scaleX = rect.height;
     }
     setMarkerPosition(position);
   }, [width, height, sectionName]);
 
   return (
     <Nav>
-      <Marker
-        initial={{ y: 0, scaleY: 0 }}
-        animate={markerPosition}
-      />
+      <Marker initial={{ x: 0, scaleX: 0 }} animate={markerPosition} />
       <ul>
         <li>
           <a onClick={() => onClick(SectionName.Bio)}>
@@ -70,32 +65,30 @@ function Wrapper({ sectionName, onClick }: Props) {
   );
 }
 
-
 const itemPadding = 16;
 const markerSize = 3;
-const markerOffset = 1;
-const markerLeft = 4;
 
 const Nav = styled.nav`
-  bottom: 0;
   font-family: var(--font-header);
+  left: 100%;
   position: fixed;
-  right: 20px;
   text-transform: uppercase;
   top: 0;
-  writing-mode: vertical-rl;
+  transform-origin: left top;
+  transform: rotate(90deg);
+  width: 100vh;
 
   ul {
     display: flex;
     justify-content: center;
     list-style: none;
     margin: 0;
-    padding: 0;
   }
 
   a {
     color: var(--color-text);
     cursor: pointer;
+    display: inline-block;
     padding: ${itemPadding}px;
     position: relative;
     text-decoration: none;
@@ -105,31 +98,30 @@ const Nav = styled.nav`
     a::after {
       background-color: var(--color-text);
       content: '';
-      height: calc(100% - ${itemPadding * 2 + markerOffset}px);
-      left: ${itemPadding - markerLeft}px;
+      height: ${markerSize}px;
+      left: ${itemPadding}px;
       opacity: 0.4;
       position: absolute;
-      top: ${itemPadding + markerOffset}px;
-      transform: scaleY(0);
-      transform-origin: top center;
+      top: calc(100% - ${itemPadding}px);
+      transform-origin: left center;
+      transform: scaleX(0);
       transition: transform 150ms ease-in;
-      width: ${markerSize}px;
+      width: calc(100% - ${itemPadding * 2}px);
     }
 
     a:hover::after {
-      transform: scaleY(1);
+      transform: scaleX(1);
     }
   }
 `;
 
 const Marker = styled(motion.div)`
   background: var(--color-text);
-  height: 1px;
+  height: ${markerSize}px;
   position: fixed;
   top: 0;
-  transform-origin: top;
-  width: ${markerSize}px;
+  transform-origin: left center;
+  width: 1px;
 `;
-
 
 export default Wrapper;
