a8a01a55ac75e07ed0ddcbc1b2b62634b1566847
Zak Johnson <me@zakj.net>
2021-05-01 14:47:42 -0700

    Bump deps and update to latest skeleton

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,22 +1,14 @@
-const sveltePreprocess = require('svelte-preprocess');
-const adapter = require('@sveltejs/adapter-static');
-const pkg = require('./package.json');
+const preprocess = require('svelte-preprocess');
+const static = require('@sveltejs/adapter-static');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: preprocess(),
   kit: {
-    adapter: adapter(),
-
+    adapter: static(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-
-    vite: {
-      ssr: {
-        noExternal: Object.keys(pkg.dependencies || {}),
-      },
-    },
   },
 };
