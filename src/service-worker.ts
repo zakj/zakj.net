c96e5c1dde8c15df28a997e3f61621205e4ad1bd
Zak Johnson <me@zakj.net>
2021-05-06 23:57:40 -0700

    Claim/skipWaiting service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,29 +1,40 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
 
 import { build, files, timestamp } from '$service-worker';
 
 const CACHE_NAME = `zakj.net-${timestamp}`;
-const URLS = ['/', '/plank', '/plank/']; // TODO trailing slashes
-const FILES = build.concat(files, URLS);
+const ROUTES = ['/', '/plank', '/plank/']; // TODO trailing slashes
+const URLS = build.concat(files, ROUTES);
 
-self.addEventListener('install', (e: ExtendableEvent) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
-});
-
-self.addEventListener('activate', (e: ExtendableEvent) => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((c) => {
-          if (c !== CACHE_NAME) return caches.delete(c);
-        })
-      );
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(URLS))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (e: FetchEvent) => {
+function clearCaches(cacheNames: string[]) {
+  return Promise.all(
+    cacheNames.map((c) => {
+      if (c !== CACHE_NAME) return caches.delete(c);
+    })
+  );
+}
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then(clearCaches)
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
