531eac480c58b784adbbfd6e611aa12e11ae5f3e
Zak Johnson <me@zakj.net>
2022-04-20 21:13:00 -0700

    Animate via transform rather than bgpos

--- src/lib/Triptych.svelte
+++ src/lib/Triptych.svelte
@@ -4,13 +4,15 @@
   export let bg: string;
   export let images: string[];
 
-  const imgBgPos = spring([
-    { x: 0.5, y: 0.5 },
-    { x: 0.5, y: 0.5 },
-    { x: 0.5, y: 0.5 },
+  const IMAGE_OVERSIZE = 0.4;
+
+  const imgPosition = spring([
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
   ]);
 
-  let startPos: { x: number; y: number } = { x: 0, y: 0 };
+  let startPosition: { x: number; y: number } = { x: 0, y: 0 };
 
   const isTouchEvent = (event: MouseEvent | TouchEvent): event is TouchEvent =>
     'touches' in event;
@@ -30,22 +32,22 @@
   function handleEnter(
     index: number
   ): (event: MouseEvent | TouchEvent) => void {
-    return (event) => (startPos = getClientCoords(event));
+    return (event) => (startPosition = getClientCoords(event));
   }
 
   function handleMove(index: number): (event: MouseEvent | TouchEvent) => void {
     return function (event: MouseEvent | TouchEvent) {
       event.preventDefault(); // prevent scrolling on mobile touchmove
-      const el = event.target as HTMLElement;
-      const box = el.getBoundingClientRect();
+      const slice = event.currentTarget as HTMLElement;
+      const box = slice.getBoundingClientRect();
       const current = getClientCoords(event);
 
-      const moveX = (current.x - startPos.x) / (box.right - box.left);
-      const moveY = (current.y - startPos.y) / (box.bottom - box.top);
-      const x = 0.5 - moveX / 2;
-      const y = 0.5 - moveY / 2;
+      const deltaX = (current.x - startPosition.x) / (box.right - box.left);
+      const deltaY = (current.y - startPosition.y) / (box.bottom - box.top);
+      const x = deltaX * box.width * (IMAGE_OVERSIZE / 2);
+      const y = deltaY * box.height * (IMAGE_OVERSIZE / 2);
 
-      imgBgPos.update((v) => {
+      imgPosition.update((v) => {
         v[index] = { x, y };
         return v;
       });
@@ -54,8 +56,8 @@
 
   function handleLeave(index: number) {
     return () =>
-      imgBgPos.update((v) => {
-        v[index] = { x: 0.5, y: 0.5 };
+      imgPosition.update((v) => {
+        v[index] = { x: 0, y: 0 };
         return v;
       });
   }
@@ -63,42 +65,50 @@
 
 <div class="container" style="--bg-img: url({bg})">
   {#each images as image, index}
+    {@const pos = $imgPosition[index]}
     <div
-      class="img"
+      class="slice"
       on:mouseenter={handleEnter(index)}
       on:mousemove={handleMove(index)}
       on:mouseleave={handleLeave(index)}
-      on:blur={handleLeave(index)}
       on:touchstart={handleEnter(index)}
       on:touchmove={handleMove(index)}
       on:touchend={handleLeave(index)}
-      style:background-image={`url(${images[index]})`}
-      style:background-position={`${$imgBgPos[index].x * 100}% ${
-        $imgBgPos[index].y * 100
-      }%`}
-    />
+    >
+      <div
+        class="img"
+        style:background-image={`url(${images[index]})`}
+        style:transform={`translate(${pos.x}px, ${pos.y}px)`}
+      />
+    </div>
   {/each}
 </div>
 
 <style>
   .container {
-    --image-width: 44%;
+    --image-scale: 140%;
+    --slice-width: 44%;
     /* background-image: linear-gradient(to bottom, #22222266, #222222ff); */
     background-image: var(--bg-img);
     background-position: center 99%; /* TODO */
     background-size: cover;
     display: flex;
-    margin-top: var(--padding);
     height: 175px;
+    margin-top: var(--padding);
+  }
+  .slice {
+    clip-path: polygon(0 100%, 39% 0, 100% 0%, 61% 100%);
+    display: grid;
+    grid: var(--image-scale) / var(--image-scale);
+    margin-left: calc((100% - var(--slice-width) * 3) / 2);
+    place-content: center;
+    width: var(--slice-width);
+  }
+  .slice:first-child {
+    margin-left: 0;
   }
   .img {
-    background-size: 140%;
-    clip-path: polygon(0 100%, 39% 0, 100% 0%, 61% 100%);
-    margin-left: calc((100% - var(--image-width) * 3) / 2);
-    width: var(--image-width);
-  }
-  .img:first-child {
-    margin-left: 0;
+    background-size: cover;
   }
 
   @media screen and (min-width: 750px) {
