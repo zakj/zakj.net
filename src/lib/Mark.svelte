0d755e7d1b213b3446269cc11b7f4cd706b662f3
Zak Johnson <me@zakj.net>
2022-10-29 15:10:21 -0700

    Clean up Mark
    * Extract tap-padding so it can be used in layout.
    * Set up browser-managed smooth scrolling.
    * Prevent default on touchend so tapping doesn't set focus/hover.
    * Factor out wrapper div

--- src/lib/Mark.svelte
+++ src/lib/Mark.svelte
@@ -12,63 +12,66 @@
   $: expanded = hovered || recentlyClicked;
 
   function handleClick() {
+    window.scrollTo({ top: 0 });
     recentlyClicked = true;
     clearTimeout(clickTimeout);
     clickTimeout = setTimeout(() => (recentlyClicked = false), 2000);
   }
 </script>
 
-<div
+<svg
+  width="200px"
+  height="217px"
+  viewBox="0 0 200 217"
   on:click={handleClick}
+  on:touchstart={handleClick}
+  on:touchend|preventDefault
   on:mouseenter={() => (hovered = true)}
   on:mouseleave={() => (hovered = false)}
 >
-  <svg width="200px" height="217px" viewBox="0 0 200 217">
-    <defs>
-      <mask id="mark-slash-mask" maskUnits="userSpaceOnUse">
-        <polygon points="0,0 120.5,0 0,202" fill="white" />
-        <polygon points="200,217 79.5,217 200,15" fill="white" />
-      </mask>
-    </defs>
+  <defs>
+    <mask id="mark-slash-mask" maskUnits="userSpaceOnUse">
+      <rect fill="white" x="0" y="0" width="200" height="217" />
+    </mask>
+    <mask id="mark-bar-mask" maskUnits="userSpaceOnUse">
+      <polygon points="0,0 120.5,0 0,202" fill="white" />
+      <polygon points="200,217 79.5,217 200,15" fill="white" />
+    </mask>
+  </defs>
 
-    <path d="M35,217 165,0" stroke-width="60" stroke-linecap="square" />
+  <path
+    d="M35,217 165,0"
+    stroke-width="60"
+    stroke-linecap="square"
+    mask="url(#mark-slash-mask)"
+  />
 
-    {#if expanded}
-      <path
-        d="M120,26.5 15,26.5"
-        stroke-width="53"
-        mask="url(#mark-slash-mask)"
-        in:draw={transitionFirst}
-        out:draw={transitionSecond}
-      />
-      <path
-        d="M75,190.5 190,190.5"
-        stroke-width="53"
-        mask="url(#mark-slash-mask)"
-        in:draw={transitionSecond}
-        out:draw={transitionFirst}
-      />
-    {/if}
-  </svg>
-</div>
+  {#if expanded}
+    <path
+      d="M120,26.5 15,26.5"
+      stroke-width="53"
+      mask="url(#mark-bar-mask)"
+      in:draw={transitionFirst}
+      out:draw={transitionSecond}
+    />
+    <path
+      d="M75,190.5 190,190.5"
+      stroke-width="53"
+      mask="url(#mark-bar-mask)"
+      in:draw={transitionSecond}
+      out:draw={transitionFirst}
+    />
+  {/if}
+</svg>
 
 <style>
-  div {
-    --tap-padding: 16px;
-    cursor: pointer;
-    left: 50%;
-    padding: var(--tap-padding);
-    position: fixed;
-    top: calc(var(--padding) - var(--tap-padding));
-    transform: translateX(
-      calc((var(--mark-size) + var(--tap-padding) * 2) / -2)
-    );
-  }
   svg {
+    box-sizing: content-box;
+    cursor: pointer;
     height: var(--mark-size);
+    padding: var(--tap-padding);
+    vertical-align: bottom;
     width: var(--mark-size);
-  }
-  path {
     stroke: var(--color-text);
   }
 </style>
