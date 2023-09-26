4136e743f6c4e7ad5661a7ceb133d5144221453d
Zak Johnson <me@zakj.net>
2023-09-25 18:03:09 -0700

    Upgrade Astro to 3.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   integrations: [svelte()],
 });
