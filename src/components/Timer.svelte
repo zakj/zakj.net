237fc4c68d71ec69208657f220cc9511fe6823cc
Zak Johnson <me@zakj.net>
2023-10-11 21:20:02 -0700

    Fix timer buttons in dark mode

--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -120,11 +120,12 @@
   }
 
   button {
-    background: none;
+    background: var(--color-white);
     border-radius: 60px;
     border: none;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
       rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+    color: var(--color-black);
     cursor: pointer;
     font-family: var(--font-family-numeric);
     font-size: fluid(16px, 20px, 320px, 800px);
