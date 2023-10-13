ea46b09c1cc99214380ff0caad6d16a01970058b
Zak Johnson <me@zakj.net>
2023-10-12 22:54:02 -0700

    Add service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import svelte from '@astrojs/svelte';
+import serviceWorker from 'astrojs-service-worker';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   build: { format: 'file' },
-  integrations: [svelte()],
+  integrations: [svelte(), serviceWorker()],
 });
